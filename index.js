5a9750fd614b0ee36ff7850ade84b8b3caf16135
Zoe Kirsh <zoekirsh@gmail.com>
2021-01-12 16:05:24 -0800

    all passing

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+let mainElement = document.querySelector('main#main')
+mainElement.remove() 
+
+let newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'Zoe is the champion'
